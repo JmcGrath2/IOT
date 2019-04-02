671d1f5c12615c70812b657ceb00dc89d18c9429
JmcGrath2 <46888710+JmcGrath2@users.noreply.github.com>
2019-04-02 21:45:25 +0100

    MoreIOTStuff

---
+++ TxtWrite/TxtWrite/TextWrite.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TxtWrite
+
+{
+    class Program
+    {
+        public static PerformanceCounter cpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        public static PerformanceCounter ramUsage = new PerformanceCounter("Memory", "Available MBytes");
+        
+       public static void Main(string[] args)
+        {
+            
+            string cpu = cpuUsage.NextValue() + "%";
+            string ram = ramUsage.NextValue() + " MB";
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                using (StreamWriter writer = new StreamWriter(@"C: \Users\User\Desktop\Data.txt"))
+                {
+                    writer.Write(cpu);
+                    writer.Write(ram);
+                }
+
+            }
+        }
+       
+        
+        
+       
+    }
+}
