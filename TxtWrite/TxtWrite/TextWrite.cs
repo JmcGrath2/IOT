a0b7cfd9769c8cc4c4d3d6a3a288e451c2360a6b
JmcGrath2 <46888710+JmcGrath2@users.noreply.github.com>
2019-05-02 20:52:57 +0100

    Final

--- TxtWrite/TxtWrite/TextWrite.cs
+++ TxtWrite/TxtWrite/TextWrite.cs
@@ -1,12 +1,11 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 
 namespace TxtWrite
 
@@ -15,29 +14,45 @@ namespace TxtWrite
     {
         public static PerformanceCounter cpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
         public static PerformanceCounter ramUsage = new PerformanceCounter("Memory", "Available MBytes");
-        public static SerialPort port;
-        
-       public static void Main(string[] args)
-        {
-            port = new SerialPort
 
+        public static string getAvailableRAM()
+        {
+            return ramUsage.NextValue() + "MB";
+        }
+
+        public static void Main(string[] args)
+        {
+            
             string cpu = cpuUsage.NextValue() + "%";
             string ram = ramUsage.NextValue() + " MB";
 
+            File.WriteAllText(@"C:\xampp\htdocs\CPURAM.txt", String.Empty);
             for (int i = 0; i < 100; i++)
             {
 
-                using (StreamWriter writer = new StreamWriter(@"C: \Users\User\Desktop\Data.txt"))
-                {
-                    writer.Write(cpu);
-                    writer.Write(ram);
-                }
+                float First = cpuUsage.NextValue();
+                System.Threading.Thread.Sleep(500);
+                Console.WriteLine("Cpu usage" + cpuUSage() + "%");
+                Console.WriteLine("Available Ram: " + getAvailableRAM());
 
+                TextWriter writer = new StreamWriter(@"C:\xampp\htdocs", append:true);
+                            
+                writer.WriteLine(cpuUSage());
+                writer.WriteLine(ram);
+                writer.Close();                
             }
+
+            //WebClient webClient = new WebClient();
+            //webClient.UploadData()
+
         }
-       
-        
-        
-       
+        public static string cpuUSage()
+        {
+            string percent = "";
+            cpuUsage.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            percent= cpuUsage.NextValue()+"%";
+            return percent;
+        }
     }
 }
